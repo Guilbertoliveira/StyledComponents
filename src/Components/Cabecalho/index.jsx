81b2ab6ec0803d805a6881a6145fdc6e267f85b6
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-05-22 23:32:54 -0300

    new component styled

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import styled from "styled-components";
 import bank_logo from "../../assets/images/bank_logo.svg";
 import {corPrimaria} from '../UI/variaveis';
+import styled from "styled-components";
+
 
 const StyledHeader = styled.nav`
   background-color: ${corPrimaria};
@@ -11,6 +12,17 @@ const StyledHeader = styled.nav`
   height: 10vh;
   align-items: center;
 `
+
+const BtnCabecalho = styled.a`
+text-align: center;
+border-radius: 3px;
+padding: 5px 20px;
+margin: 0 10px;
+font-weight: 600;
+border: 2px solid white;
+background: ${(props) => props.primary ? "white": corPrimaria};
+color: ${(props) => props.primary ? corPrimaria : "white"};
+`;
 
 const Logo = styled.img`
 height: 50px;
@@ -22,12 +34,12 @@ const Cabecalho = () => {
     <StyledHeader>
       <Logo src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <BtnCabecalho primary href="https://google.com">
           Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </BtnCabecalho>
+        <BtnCabecalho href="https://google.com">
           Sair
-        </a>
+        </BtnCabecalho>
       </div>
     </StyledHeader>
   );
